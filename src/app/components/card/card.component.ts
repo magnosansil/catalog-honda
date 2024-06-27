f873f9188717a8c622f4fcf13fcfaed6c3702ec9
magnosansil <magnosansil@outlook.com>
2024-06-27 00:22:53 -0300

    Improves LCP With Image Preload

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -9,10 +9,22 @@ import { sellerInfo } from '../../data/seller-data';
 })
 
 export class CardComponent {
-  @Input() image: String = '';
+  @Input() image: string = '';
   @Input() name: String = '';
   @Input() value: Number = 0;
   @Input() portion: String | undefined = '';
   
   seller = sellerInfo;
+
+  ngOnInit() {
+    this.preloadImage(this.image);
+  }
+
+  private preloadImage(url: string) {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => {
+      console.log(`Imagem pré-carregada: ${url}`);
+    }
+  }
 }
